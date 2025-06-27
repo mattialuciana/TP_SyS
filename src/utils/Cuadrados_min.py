2f6487f4669d5ed6fdd75651f84986544db0e28b
Luciana <mattialuciana@gmail.com>
2025-06-26 22:41:29 -0300

    Agrega función Lundeby y corrige algunas cosas

--- src/utils/Cuadrados_min.py
+++ src/utils/Cuadrados_min.py
@@ -36,4 +36,31 @@ def cuadrados_minimos(señal, fs=44100):
     
     return f, m, b
 
+def cuadrados_minimos_gen(x, y):
+    """
+    Ajusta una línea recta a los datos (x, y) usando el método de mínimos cuadrados.
+    Devuelve la pendiente (m) y la intersección (b) de la línea ajustada.
+    Parámetros:
+    x : array
+        Valores de la variable independiente.
+    y : array
+        Valores de la variable dependiente.
+    Devuelve:
+    f : array   
+        Valores de la función ajustada.
+    m : float
+        Pendiente de la recta ajustada.
+    b : float
+        Intersección de la recta ajustada con el eje y.
+    """
 
+    A = np.sum(x)
+    B = np.sum(y)
+    C = np.sum(x**2)
+    D = np.sum(x * y)
+
+    b = (B - (D*A)/C)/(len(x) - (A**2)/C)
+    m = (D - b*A)/C
+    f = m*x + b
+
+    return f, m, b
